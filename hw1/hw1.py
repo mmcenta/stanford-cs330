5ab0b9c134d99bd924dd712d68bbdc028df263d2
Matheus Centa <matheuscenta@gmail.com>
2020-06-04 11:41:02 +0200

    Add Convolution to HW1

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -7,7 +7,7 @@ import random
 import tensorflow as tf
 from load_data import DataGenerator
 from tensorflow.python.platform import flags
-from tensorflow.keras import layers
+from tensorflow.keras.layers import LSTM, ConvLSTM2D, TimeDistributed, MaxPool2D, Flatten
 
 
 FLAGS = flags.FLAGS
@@ -33,8 +33,17 @@ flags.DEFINE_string('label', '',
 flags.DEFINE_integer('n_layers', 1,
                      'Number of hidden layers in the model.')
 
-flags.DEFINE_integer('layer_size', 128,
+flags.DEFINE_integer('layer_units', 128,
                      'Number of units in the hidden layers.')
+
+flags.DEFINE_bool('cnn', False,
+                  'Whether to use the CNN-based architecture.')
+
+flags.DEFINE_float('lr', 0.001,
+                   'Learning rate.')
+
+flags.DEFINE_float('dropout', 0.1,
+                  'Whether to use dropout.')
 
 
 def loss_function(preds, labels):
@@ -64,9 +73,8 @@ class MANN(tf.keras.Model):
 
         self.hidden_layers = []
         for _ in range(n_layers):
-            self.hidden_layers.append(tf.keras.layers.LSTM(layer_units,
-                return_sequences=True))
-        self.output_layer = tf.keras.layers.LSTM(num_classes, return_sequences=True)
+            self.hidden_layers.append(LSTM(layer_units, return_sequences=True))
+        self.output_layer = LSTM(num_classes, return_sequences=True)
 
     def call(self, input_images, input_labels):
         """
@@ -79,7 +87,7 @@ class MANN(tf.keras.Model):
         """
         #############################
         #### YOUR CODE GOES HERE ####
-        B, K, N, D = input_images.shape
+        _, K, N, D = input_images.shape
         K -= 1
 
         # zero the last labels per batch (no slice assign on this tf version)
@@ -102,19 +110,84 @@ class MANN(tf.keras.Model):
         #############################
         return out
 
-ims = tf.placeholder(tf.float32, shape=(
-    None, FLAGS.num_samples + 1, FLAGS.num_classes, 784))
+
+class ConvMANN(tf.keras.Model):
+    def __init__(self, num_classes, samples_per_class, dropout=0.1):
+        super(ConvMANN, self).__init__()
+
+        self.num_classes = num_classes
+        self.samples_per_class = samples_per_class
+
+        self.conv1 = ConvLSTM2D(16, 3, activation='relu', dropout=dropout,
+            recurrent_dropout=2 * dropout, return_sequences=True)
+        self.pool1 = TimeDistributed(MaxPool2D())
+
+        self.conv2 = ConvLSTM2D(32, 3, activation='relu', dropout=dropout,
+            recurrent_dropout=2 * dropout, return_sequences=True)
+        self.pool2 = TimeDistributed(MaxPool2D())
+
+
+        self.flatten = TimeDistributed(Flatten())
+
+        self.lstm1 = LSTM(256, return_sequences=True)
+        self.lstm2 = LSTM(num_classes, return_sequences=True)
+
+    def call(self, input_images, input_labels):
+        _, K, N, D, _ = input_images.shape
+        K -= 1
+
+        # zero the last labels per batch (no slice assign on this tf version)
+        input_labels = tf.concat(
+            (input_labels[:, :-1], tf.zeros_like(input_labels[:, -1:])), axis=1)
+
+        # feed the input images to the CNN layers
+        input_images = tf.reshape(input_images,
+            (-1, (K + 1) * N, D, D)) # unroll
+        input_images = tf.expand_dims(input_images,
+            axis=-1)
+
+        x_img = self.conv1(input_images)
+        x_img = self.pool1(x_img)
+        x_img = self.conv2(x_img)
+        x_img = self.pool2(x_img)
+
+        x_img = self.flatten(x_img)
+
+        # append label vectors to feature vector
+        input_labels = tf.reshape(input_labels,
+            (-1, (K + 1) * N, N)) # unroll
+        x = tf.concat((x_img, input_labels), axis=-1)
+
+        # feed the feature vector to the LSTM
+        x = self.lstm1(x)
+        out = self.lstm2(x)
+
+        # reshape to match the output shape
+        out = tf.reshape(out, (-1, K + 1, N, N))
+        return out
+
+
+if FLAGS.cnn:
+    ims = tf.placeholder(tf.float32, shape=(
+        None, FLAGS.num_samples + 1, FLAGS.num_classes, 28, 28))
+else:
+    ims = tf.placeholder(tf.float32, shape=(
+        None, FLAGS.num_samples + 1, FLAGS.num_classes, 784))
 labels = tf.placeholder(tf.float32, shape=(
     None, FLAGS.num_samples + 1, FLAGS.num_classes, FLAGS.num_classes))
 
 data_generator = DataGenerator(
-    FLAGS.num_classes, FLAGS.num_samples + 1)
+    FLAGS.num_classes, FLAGS.num_samples + 1, config={"flatten": not FLAGS.cnn})
 
-o = MANN(FLAGS.num_classes, FLAGS.num_samples + 1)
+if FLAGS.cnn:
+    o = ConvMANN(FLAGS.num_classes, FLAGS.num_samples + 1, FLAGS.dropout)
+else:
+    o = MANN(FLAGS.num_classes, FLAGS.num_samples + 1,
+        n_layers=FLAGS.n_layers, layer_units=FLAGS.layer_units)
 out = o(ims, labels)
 
 loss = loss_function(out, labels)
-optim = tf.train.AdamOptimizer(0.001)
+optim = tf.train.AdamOptimizer(FLAGS.lr)
 optimizer_step = optim.minimize(loss)
 
 logs = defaultdict(list) # minimalist logging for plots
